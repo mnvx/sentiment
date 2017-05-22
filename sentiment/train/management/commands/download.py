f91c0ccdf2c81be8edfab2ddc5cf16f42e80aaef
mnv <mnvx@yandex.ru>
2017-05-22 19:49:34 +0300

    download

--- sentiment/train/management/commands/download.py
+++ sentiment/train/management/commands/download.py
@@ -1,8 +1,6 @@
 from django.core.management.base import BaseCommand
-import urllib.request
 import os
-# import wget
-# import dropbox
+import subprocess
 
 
 class Command(BaseCommand):
@@ -23,16 +21,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.download(options['source'], 'positive')
         self.download(options['source'], 'negative')
-
-        self.stdout.write('source: %s' % options['source'])
-        self.stdout.write(self.style.SUCCESS('Successfully'))
+        self.stdout.write(self.style.SUCCESS('Success'))
 
     def download(self, source: str, sentiment: str):
         path = os.path.dirname(
             os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
         path = os.path.join(path, 'database', 'raw', source)
+        file = os.path.join(path, sentiment + '.csv')
         url = self.urls[source][sentiment];
-        urllib.request.urlretrieve(url, os.path.join(path, sentiment + '.csv'))
-        # wget.download(url, path)
-        # dbx = dropbox.Dropbox
-        # dbx.files_download_to_file(path, url)
+        subprocess.call(['wget', url, '-O', file])
--- sentiment/train/management/commands/train.py
+++ sentiment/train/management/commands/train.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+import csv
 
 
 class Command(BaseCommand):
